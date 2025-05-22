6d136248b84174a525cac424533d6285e75ec159
Aldo Flores <sarduts@gmail.com>
2025-05-21 23:12:22 -0600

    crea clase Principal

--- src/main/java/com/aluracursos/Screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/Screenmatch/ScreenmatchApplication.java
@@ -3,6 +3,7 @@ package com.aluracursos.Screenmatch;
 import com.aluracursos.Screenmatch.model.DatosEpisodio;
 import com.aluracursos.Screenmatch.model.DatosSerie;
 import com.aluracursos.Screenmatch.model.DatosTemporadas;
+import com.aluracursos.Screenmatch.principal.Principal;
 import com.aluracursos.Screenmatch.service.ConsumoAPI;
 import com.aluracursos.Screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
@@ -21,23 +22,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoAPI();
-		var json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=1432478b");
-		//var json = consumoApi.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-		ConvierteDatos conversor = new ConvierteDatos();
-		var datos = conversor.obtenerDatos(json, DatosSerie.class);
-		System.out.println(datos);
-		json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=1432478b");
-		DatosEpisodio episodio = conversor.obtenerDatos(json, DatosEpisodio.class);
-		System.out.println(episodio);
-
-		List<DatosTemporadas> temporadas = new ArrayList<>();
-		for (int i = 1; i <= datos.totalDeTemporadas(); i++) {
-			json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=" + i + "&apikey=1432478b");
-			var datosTemporadas = conversor.obtenerDatos(json,DatosTemporadas.class);
-			temporadas.add(datosTemporadas);
-		}
-		temporadas.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.muestraMenu();
 	}
 }
