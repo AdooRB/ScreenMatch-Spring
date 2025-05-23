5583dbbb48b5af033b710a18580b8b351f75b36f
Aldo Flores <sarduts@gmail.com>
2025-05-23 17:22:05 -0600

    analíza episodios para sacar el Top 5

--- src/main/java/com/aluracursos/Screenmatch/principal/Principal.java
+++ src/main/java/com/aluracursos/Screenmatch/principal/Principal.java
@@ -7,22 +7,24 @@ import com.aluracursos.Screenmatch.service.ConsumoAPI;
 import com.aluracursos.Screenmatch.service.ConvierteDatos;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner teclado = new Scanner(System.in);
     private ConsumoAPI consumoApi = new ConsumoAPI();
     private final String URL_BASE = "https://www.omdbapi.com/?t=";
-    private final String API_KEY =  "&apikey=1432478b";
+    private final String API_KEY = "&apikey=1432478b";
     private ConvierteDatos conversor = new ConvierteDatos();
 
-    public void muestraMenu(){
+    public void muestraMenu() {
         System.out.println("Por favor escribe el nombre de la seria que quieras ingresar: ");
 
         //Busca los datos generales de la serie
         var nombreSerie = teclado.nextLine();
-        var json = consumoApi.obtenerDatos(URL_BASE + nombreSerie.replace(" ","+") + API_KEY);
+        var json = consumoApi.obtenerDatos(URL_BASE + nombreSerie.replace(" ", "+") + API_KEY);
         var datos = conversor.obtenerDatos(json, DatosSerie.class);
         System.out.println(datos);
 
@@ -30,7 +32,7 @@ public class Principal {
         List<DatosTemporadas> temporadas = new ArrayList<>();
         for (int i = 1; i <= datos.totalDeTemporadas(); i++) {
             json = consumoApi.obtenerDatos(URL_BASE + nombreSerie.replace(" ", "+") + "&Season=" + i + API_KEY);
-            var datosTemporadas = conversor.obtenerDatos(json,DatosTemporadas.class);
+            var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
             temporadas.add(datosTemporadas);
         }
         //Mostrar los objetos DatosTemporada
@@ -47,9 +49,21 @@ public class Principal {
         }*/
 
         //Mostrar los episodios por "función lambda"
-            temporadas.forEach(t ->{
-                System.out.println("************Temporada " + t.numero() + " ***************");
-                t.episodios().forEach(e -> System.out.println(e.titulo()));
-            });
+        temporadas.forEach(t -> {
+            System.out.println("************Temporada " + t.numero() + " ***************");
+            t.episodios().forEach(e -> System.out.println(e.titulo()));
+        });
+//		Covertir todas las informaciones a una lista del tipo DatosEpisodios
+        List<DatosEpisodio> datosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        //Top 5
+        System.out.println("Top 5 episodios");
+        datosEpisodios.stream()
+                .filter(e -> !e.evaluacion().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DatosEpisodio::evaluacion).reversed())
+                .limit(5)
+                .forEach(System.out::println);
     }
 }
