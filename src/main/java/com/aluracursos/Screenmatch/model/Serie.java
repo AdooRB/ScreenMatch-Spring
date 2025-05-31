2234bf8a258507e546ae219f098ecf1ec8826d5d
Aldo Flores <sarduts@gmail.com>
2025-05-30 22:30:49 -0600

    crea clase Serie

---
+++ src/main/java/com/aluracursos/Screenmatch/model/Serie.java
@@ -0,0 +1,13 @@
+package com.aluracursos.Screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public class Serie {
+    private String titulo;
+    private Integer totalDeTemporadas;
+    private Double evaluacion;
+    private String actores;
+    private Categoria genero;
+    private String sinopsis;
+    private String poster;
+}
