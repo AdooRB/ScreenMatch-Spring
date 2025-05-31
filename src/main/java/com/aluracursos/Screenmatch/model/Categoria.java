4a11d4785a102ad933956cdae1515fd786f2dbf2
Aldo Flores <sarduts@gmail.com>
2025-05-31 16:50:01 -0600

    añade método_static 'fromString'

--- src/main/java/com/aluracursos/Screenmatch/model/Categoria.java
+++ src/main/java/com/aluracursos/Screenmatch/model/Categoria.java
@@ -3,13 +3,22 @@ package com.aluracursos.Screenmatch.model;
 public enum Categoria {
     ACCION("Action"),
     ROMANCE("Romance"),
-    Comedia("Comedy"),
+    COMEDIA("Comedy"),
     DRAMA("Drama"),
     CRIMEN("Crime");
 
     private String categoriaOmbd;
 
     Categoria (String categoriaOmbd){
-        this.categoriaOmbd = categoriaOmbd
+        this.categoriaOmbd = categoriaOmbd;
+    }
+
+    public static Categoria fromString(String text){
+        for (Categoria categoria : Categoria.values()){
+            if (categoria.categoriaOmbd.equalsIgnoreCase(text)){
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Ninguna categoría encontrada: " + text);
     }
 }
