a0d94892b25ad250f6bcf94d47490c3946847036
Aldo Flores <sarduts@gmail.com>
2025-05-31 16:52:05 -0600

    añade 'constructor', 'getters', 'setters' y 'toString'

--- src/main/java/com/aluracursos/Screenmatch/model/Serie.java
+++ src/main/java/com/aluracursos/Screenmatch/model/Serie.java
@@ -2,6 +2,8 @@ package com.aluracursos.Screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 
+import java.util.OptionalDouble;
+
 public class Serie {
     private String titulo;
     private Integer totalDeTemporadas;
@@ -10,4 +12,80 @@ public class Serie {
     private Categoria genero;
     private String sinopsis;
     private String poster;
+
+    public Serie(DatosSerie datosSerie){
+        this.titulo = datosSerie.titulo();
+        this.totalDeTemporadas = datosSerie.totalDeTemporadas();
+        this.evaluacion = OptionalDouble.of(Double.valueOf(datosSerie.evaluacion())).orElse(0.0);
+        this.actores = datosSerie.actores();
+        this.genero = Categoria.fromString(datosSerie.genero().split(",")[0].trim());
+        this.sinopsis = datosSerie.sinopsis();
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getTotalDeTemporadas() {
+        return totalDeTemporadas;
+    }
+
+    public void setTotalDeTemporadas(Integer totalDeTemporadas) {
+        this.totalDeTemporadas = totalDeTemporadas;
+    }
+
+    public Double getEvaluacion() {
+        return evaluacion;
+    }
+
+    public void setEvaluacion(Double evaluacion) {
+        this.evaluacion = evaluacion;
+    }
+
+    public String getActores() {
+        return actores;
+    }
+
+    public void setActores(String actores) {
+        this.actores = actores;
+    }
+
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Categoria genero) {
+        this.genero = genero;
+    }
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    @Override
+    public String toString() {
+        return "titulo='" + titulo + '\'' +
+                ", totalDeTemporadas=" + totalDeTemporadas +
+                ", evaluacion=" + evaluacion +
+                ", actores='" + actores + '\'' +
+                ", genero=" + genero +
+                ", sinopsis='" + sinopsis + '\'' +
+                ", poster='" + poster + '\'';
+    }
 }
